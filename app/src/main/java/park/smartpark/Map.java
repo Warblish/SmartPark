edba48ba4fc168d6f9b028b69029901262f11a2c
Ironarcher <arpad.kovesdy@gmail.com>
2017-09-23 15:02:05 -0700

    Finished pay icon + auto zoomed on markers

--- app/src/main/java/park/smartpark/Map.java
+++ app/src/main/java/park/smartpark/Map.java
@@ -3,14 +3,17 @@ package park.smartpark;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.json.JSONArray;
@@ -43,6 +46,34 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback {
         setSupportActionBar(myToolbar);
     }
 
+    /*
+    Following functions handle the action bar
+    onCreateOptionsMenu sets up the action bar and adds any buttons from res/menu/main_menu.xml
+    onOptionsItemSelected determines what occurs when the action bar button is clicked
+     */
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here.
+
+        switch(item.getItemId()){
+            case(R.id.action_settings):
+                //Handle if the settings button is pressed (nothing happens right now)
+                return true;
+            case(R.id.action_pay):
+                //Handle when payment icon is pressed
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     /**
      * Manipulates the map once available.
@@ -103,6 +134,59 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback {
                 mMap.addMarker(m);
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(m.getPosition()));
             }
+            //Find the extreme bounds for LatLng (get the northern most, southern most, eastern most, western most points on the map)
+            double[] extreme_coords = new double[4]; //Northern, southern, eastern, western
+            for(int i = 0; i<4; i++) {
+                for (MarkerOptions m : result) {
+                    if (extreme_coords[i] == 0){
+                        //The most extreme coordinate has not been set so just set it to the first marker in the series
+                        if(i==0 || i==1){
+                            extreme_coords[i] = m.getPosition().latitude;
+                        } else if(i==2 || i==3){
+                            extreme_coords[i] = m.getPosition().longitude;
+                        }
+                    } else{
+                        switch(i){
+                            case 0:
+                                //Most nothern
+                                if(m.getPosition().latitude > extreme_coords[i]){
+                                    extreme_coords[i] = m.getPosition().latitude;
+                                }
+                                break;
+                            case 1:
+                                //Most southern
+                                if(m.getPosition().latitude < extreme_coords[i]){
+                                    extreme_coords[i] = m.getPosition().latitude;
+                                }
+                                break;
+                            case 2:
+                                //Most eastern
+                                if(m.getPosition().longitude > extreme_coords[i]){
+                                    extreme_coords[i] = m.getPosition().longitude;
+                                }
+                                break;
+                            case 3:
+                                //Most western
+                                if(m.getPosition().longitude < extreme_coords[i]){
+                                    extreme_coords[i] = m.getPosition().longitude;
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+            //Print values to console for debugging purposes
+            Log.wtf("northern most extreme bound", Double.toString(extreme_coords[0]));
+            Log.wtf("southern most extreme bound", Double.toString(extreme_coords[1]));
+            Log.wtf("eastern most extreme bound", Double.toString(extreme_coords[2]));
+            Log.wtf("western most extreme bound", Double.toString(extreme_coords[3]));
+            //Create bounds for the camera so the map encompasses the whole campus, but is still zoomed in
+            //Create new LatLng objects for the two corners (northwest and southeast)
+            LatLng northeast = new LatLng(extreme_coords[0], extreme_coords[2]);
+            LatLng southwest = new LatLng(extreme_coords[1], extreme_coords[3]);
+            //Create new LatLngBounds object
+            LatLngBounds bounds =  new LatLngBounds(southwest, northeast);
+            mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 100));
             Log.wtf("test", "cp6");
         }
     }
